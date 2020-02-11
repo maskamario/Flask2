614a0ce3ace7901338ace2e0e3f691019939b57d
maskamario <maskamario@gmail.com>
2020-02-11 22:50:32 +0200

    indexi lisamine

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hi_maska():
-    return "Hi Maska, I'm alive"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
